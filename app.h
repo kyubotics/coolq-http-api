e70f22c831dfbf60e3a3133127f70bcd3a6ea5bc
Richard Chien <richardchienthebest@gmail.com>
2017-01-30 21:55:24 +0800

    Change version number

--- app.h
+++ app.h
@@ -1,5 +1,5 @@
 #define CQAPPID "io.github.richardchien.coolqhttpapi"
 #define CQAPPINFO CQAPIVERTEXT "," CQAPPID
 #define CQAPPNAME "CoolQ HTTP API Plugin"
-#define CQAPPVER "v1.0.0"
+#define CQAPPVER "v1.0.1"
 #define CQAPPFULLNAME CQAPPNAME " " CQAPPVER
