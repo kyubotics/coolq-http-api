dab4114ed43cf4c117de8d3dda15900c35af5587
Richard Chien <richardchienthebest@gmail.com>
2017-03-22 12:44:56 +0800

    Update docs

--- docs/config.js
+++ docs/config.js
@@ -1,27 +1,24 @@
-self.$config = {
+docute.init({
     title: 'CoolQ HTTP API 插件文档',
     home: 'https://raw.githubusercontent.com/richardchien/coolq-http-api/master/README.md',
     repo: 'richardchien/coolq-http-api',
-    url: 'https://richardchien.github.io/coolq-http-api',
-    nav: {
-        default: [
-            {
-                title: '首页', path: '/'
-            },
-            {
-                title: '配置文件说明', path: '/Configuration'
-            },
-            {
-                title: 'API 列表', path: '/API'
-            },
-            {
-                title: '上报数据格式', path: '/Post'
-            },
-            {
-                title: 'CQ 码', path: '/CQCode'
-            }
-        ]
-    },
+    nav: [
+        {
+            title: '首页', path: '/'
+        },
+        {
+            title: '配置文件说明', path: '/Configuration'
+        },
+        {
+            title: 'API 列表', path: '/API'
+        },
+        {
+            title: '上报数据格式', path: '/Post'
+        },
+        {
+            title: 'CQ 码', path: '/CQCode'
+        }
+    ],
     tocVisibleDepth: 2,
     plugins: []
-};
+});
