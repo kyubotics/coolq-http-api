ab90d47787a6121fd27108f3e89a2d88734624e6
Richard Chien <richardchienthebest@gmail.com>
2017-06-08 14:14:15 +0800

    Fix bug of message encoding

--- src/app.h
+++ src/app.h
@@ -3,5 +3,5 @@
 #define CQAPPID "io.github.richardchien.coolqhttpapi"
 #define CQAPPINFO CQAPIVERTEXT "," CQAPPID
 #define CQAPPNAME "CoolQ HTTP API Plugin"
-#define CQAPPVER "v1.1.3"
+#define CQAPPVER "v1.1.4"
 #define CQAPPFULLNAME CQAPPNAME " " CQAPPVER
