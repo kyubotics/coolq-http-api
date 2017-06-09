907ffab4ca0f4a920778371c6085209445e27d1e
Richard Chien <richardchienthebest@gmail.com>
2017-06-09 13:54:02 +0800

    Move to 1.1.5

--- src/app.h
+++ src/app.h
@@ -3,5 +3,5 @@
 #define CQ_APP_ID "io.github.richardchien.coolqhttpapi"
 #define CQ_APP_INFO CQAPIVERTEXT "," CQ_APP_ID
 #define CQ_APP_NAME "CoolQ HTTP API Plugin"
-#define CQ_APP_VERSION "v1.1.4"
+#define CQ_APP_VERSION "v1.1.5"
 #define CQ_APP_FULLNAME CQ_APP_NAME " " CQ_APP_VERSION
