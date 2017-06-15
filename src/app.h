b118baf804a664efad1d7ed7618d3b06bf722b74
Richard Chien <richardchienthebest@gmail.com>
2017-06-15 22:23:50 +0800

    Add version

--- src/app.h
+++ src/app.h
@@ -24,7 +24,7 @@
 #define CQ_APP_ID "io.github.richardchien.coolqhttpapi"
 #define CQ_APP_INFO CQAPIVERTEXT "," CQ_APP_ID
 #define CQ_APP_NAME "CoolQ HTTP API Plugin"
-#define CQ_APP_VERSION "v1.1.5"
+#define CQ_APP_VERSION "v2.1.0"
 #define CQ_APP_FULLNAME CQ_APP_NAME " " CQ_APP_VERSION
 
 #include "common.h"
