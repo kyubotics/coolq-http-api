2433dc36b7c431e3ecf29bca04301c1071726ed0
Richard Chien <richardchienthebest@gmail.com>
2017-07-04 13:54:52 +0800

    Add message parser

--- src/message.cpp
+++ src/message.cpp
@@ -25,10 +25,153 @@
 
 using namespace std;
 
-Message::Message(const str &msg_str) : msg_str_(msg_str), msg_json_(nullptr) {}
+/**
+ * Implement a FSM manually,
+ * because the regex lib of VC++ will throw stack overflow in some cases.
+ */
+static vector<Message::Segment> parse(const str &raw_msg) {
+    vector<Message::Segment> segments;
+    const static auto TEXT = 0;
+    const static auto FUNCTION_NAME = 1;
+    const static auto PARAMS = 2;
+    auto state = TEXT;
+    auto end = raw_msg.c_end();
+    stringstream text_s, function_name_s, params_s;
+    auto curr_cq_start = end;
+    for (auto it = raw_msg.c_begin(); it != end; ++it) {
+        auto curr = *it;
+        switch (state) {
+        case TEXT: {
+        text:
+            if (curr == '[' && end - it >= 5 /* [CQ:a] at least 5 chars behind */
+                && *(it + 1) == 'C' && *(it + 2) == 'Q' && *(it + 3) == ':') {
+                state = FUNCTION_NAME;
+                curr_cq_start = it;
+                it += 3;
+            } else {
+                text_s << curr;
+            }
+            break;
+        }
+        case FUNCTION_NAME: {
+            if (curr >= 'A' && curr <= 'Z'
+                || curr >= 'a' && curr <= 'z'
+                || curr >= '0' && curr <= '9') {
+                function_name_s << curr;
+            } else if (curr == ',') {
+                // function name out, params in
+                state = PARAMS;
+            } else if (curr == ']') {
+                // CQ code end, with no params
+                goto params;
+            } else {
+                // unrecognized character
+                text_s << string(curr_cq_start, it); // mark as text
+                curr_cq_start = end;
+                function_name_s = stringstream();
+                params_s = stringstream();
+                state = TEXT;
+                // because the current char may be '[', so we goto text part
+                goto text;
+            }
+            break;
+        }
+        case PARAMS: {
+        params:
+            if (curr == ']') {
+                // CQ code end
+                Message::Segment seg;
 
-Message::Message(json_t *msg_json) : msg_json_(nullptr) {
+                seg.type = function_name_s.str();
+                while (params_s.rdbuf()->in_avail()) {
+                    // split key and value
+                    string key, value;
+                    getline(params_s, key, '=');
+                    getline(params_s, value, ',');
+                    seg.data[key] = message_unescape(value);
+                }
+
+                if (text_s.rdbuf()->in_avail()) {
+                    // there is a text segment before this CQ code
+                    segments.push_back(Message::Segment{"text",{{"text", message_unescape(text_s.str())}}});
+                    text_s = stringstream();
+                }
+
+                segments.push_back(seg);
+                curr_cq_start = end;
+                text_s = stringstream();
+                function_name_s = stringstream();
+                params_s = stringstream();
+                state = TEXT;
+            } else {
+                params_s << curr;
+            }
+        }
+        default: break;
+        }
+    }
+
+    // iterator end, there may be some rest of message we haven't put into segments
+    switch (state) {
+    case FUNCTION_NAME:
+    case PARAMS:
+        // we are in CQ code, but it ended with no ']', so it's a text segment
+        text_s << string(curr_cq_start, end);
+        // should fall through
+    case TEXT:
+        if (text_s.rdbuf()->in_avail()) {
+            segments.push_back(Message::Segment{"text",{{"text", text_s.str()}}});
+        }
+    default: break;
+    }
+
+    return segments;
+}
+
+Message::Message(const str &msg_str) : msg_str_(msg_str), msg_json_(nullptr) {
+    this->segments_ = parse(msg_str);
+}
+
+Message::Message(json_t *msg_json) : msg_json_(msg_json) {
     json_incref(msg_json);
+    if (json_is_string(msg_json)) {
+        this->segments_ = parse(str(json_string_value(msg_json)));
+    } else if (json_is_array(msg_json)) {
+        size_t i;
+        json_t *seg;
+        json_array_foreach(this->msg_json_, i, seg) {
+            if (!json_is_object(seg)) {
+                continue;
+            }
+            auto type_json = json_object_get(seg, "type");
+            auto data_json = json_object_get(seg, "data");
+            if (!json_is_string(type_json) 
+                || data_json && !json_is_null(data_json) && !json_is_object(data_json)) {
+                // type is not a string,
+                // or data is neither null nor an object,
+                // we think this segment is invalid
+                continue;
+            }
+
+            str type = json_string_value(type_json);
+            map<str, str> data;
+            if (json_is_object(data_json)) {
+                const char *key;
+                json_t *value;
+                json_object_foreach(data_json, key, value) {
+                    if (!key || strlen(key) == 0) {
+                        continue;
+                    }
+                    str v;
+                    if (json_is_string(value)) {
+                        v = json_string_value(value);
+                    }
+                    data[key] = v;
+                }
+            }
+            this->segments_.push_back(Segment{type, data});
+        }
+    }
 }
 
 Message::Message(const Message &other) {
@@ -39,7 +182,9 @@ Message::Message(const Message &other) {
 }
 
 Message::~Message() {
-    json_decref(this->msg_json_);
+    if (this->msg_json_) {
+        json_decref(this->msg_json_);
+    }
 }
 
 str Message::process_outcoming() const {
