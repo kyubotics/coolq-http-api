ea83c0965f5f5ad54fb56f7b28f29ffddead8b5c
Richard Chien <richardchienthebest@gmail.com>
2017-10-15 22:31:44 +0800

    Remove ResponseHelper class

--- src/api/server_class.cpp
+++ src/api/server_class.cpp
@@ -31,27 +31,10 @@ using Request = HttpServer::Request;
 
 extern ApiHandlerMap api_handlers; // defined in handlers.cpp
 
-class ResponseHelper {
-public:
-    unsigned short status_code;
-    bytes body;
-    SimpleWeb::CaseInsensitiveMultimap headers;
-
-    void write(const shared_ptr<Response> response) {
-        *response << "HTTP/1.1 " << SimpleWeb::status_code(SimpleWeb::StatusCode(status_code)) << "\r\n";
-        headers.erase("Content-Length");
-        headers.emplace("Content-Length", to_string(body.size()));
-        for (const auto &kv : headers) {
-            *response << kv.first << ": " << kv.second << "\r\n";
-        }
-        *response << "\r\n" << body;
-    }
-};
-
 static const auto TAG = u8"API服务";
 
 static bool authorize(const decltype(Request::header) &headers, const json &query_args,
-                      const function<void(unsigned short)> on_failed = nullptr) {
+                      const function<void(SimpleWeb::StatusCode)> on_failed = nullptr) {
     if (config.access_token.empty()) {
         return true;
     }
@@ -66,14 +49,14 @@ static bool authorize(const decltype(Request::header) &headers, const json &quer
 
     if (token_given.empty()) {
         if (on_failed) {
-            on_failed(401 /* Unauthorized */);
+            on_failed(SimpleWeb::StatusCode::client_error_unauthorized);
         }
         return false;
     }
 
     if (token_given != config.access_token) {
         if (on_failed) {
-            on_failed(403 /* Forbidden */);
+            on_failed(SimpleWeb::StatusCode::client_error_forbidden);
         }
         return false;
     }
@@ -87,7 +70,7 @@ void ApiServer::init() {
     server_.default_resource["GET"]
             = server_.default_resource["POST"]
             = [](shared_ptr<Response> response, shared_ptr<Request> request) {
-                ResponseHelper{404 /* Not Found */}.write(response);
+                response->write(SimpleWeb::StatusCode::client_error_not_found);
             };
 
     for (const auto &handler_kv : api_handlers) {
@@ -101,7 +84,7 @@ void ApiServer::init() {
                     json args = request->parse_query_string(), form;
 
                     auto authorized = authorize(request->header, args, [&response](auto status_code) {
-                        ResponseHelper{status_code}.write(response);
+                        response->write(status_code);
                     });
                     if (!authorized) {
                         Log::d(TAG, u8"没有提供 Token 或 Token 不符，已拒绝请求");
@@ -126,12 +109,12 @@ void ApiServer::init() {
                                 }
                             } catch (invalid_argument &) {
                                 Log::d(TAG, u8"HTTP 正文的 JSON 无效或者不是对象");
-                                ResponseHelper{400 /* Bad Request */}.write(response);
+                                response->write(SimpleWeb::StatusCode::client_error_bad_request);
                                 return;
                             }
                         } else {
                             Log::d(TAG, u8"Content-Type 不支持");
-                            ResponseHelper{406 /* Not Acceptable */}.write(response);
+                            response->write(SimpleWeb::StatusCode::client_error_not_acceptable);
                             return;
                         }
                     }
@@ -161,6 +144,10 @@ void ApiServer::init() {
                     Log::i(TAG, u8"已成功处理一个 API 请求：" + request->path);
                 };
     }
+
+    server_.resource["^/data/(.*)$"]["GET"] = [](shared_ptr<Response> response, shared_ptr<Request> request) {
+        response->write(SimpleWeb::StatusCode::success_ok);
+    };
 
     initiated_ = true;
 }
