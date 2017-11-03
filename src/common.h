8d9a40f3195733b82ea99df4d0994308b349666c
Richard Chien <richardchienthebest@gmail.com>
2017-11-03 12:21:38 +0800

    Move "WIN32_LEAN_AND_MEAN" macro to project property

--- src/common.h
+++ src/common.h
@@ -20,7 +20,6 @@
 #pragma once
 
 #include <SDKDDKVer.h>
-#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
 #include <algorithm>
--- src/utils/encoding.h
+++ src/utils/encoding.h
@@ -19,7 +19,6 @@
 
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
 #include <string>
