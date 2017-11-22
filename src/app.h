0faf66f34413acec8e2950b0925de466ce30d03b
Richard Chien <richardchienthebest@gmail.com>
2017-11-22 10:26:55 +0800

    Bump to 3.3.1

--- src/app.h
+++ src/app.h
@@ -21,8 +21,8 @@
 
 #define CQAPP_ID "io.github.richardchien.coolqhttpapi"
 #define CQAPP_NAME "CoolQ HTTP API"
-#define CQAPP_VERSION "3.3.0"
-#define CQAPP_BUILD_NUMBER 315
+#define CQAPP_VERSION "3.3.1"
+#define CQAPP_BUILD_NUMBER 316
 #define CQAPP_FULLNAME CQAPP_NAME " v" CQAPP_VERSION
 #define CQAPP_USER_AGENT "CoolQHttpApi/" CQAPP_VERSION
 
