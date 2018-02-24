9c9d9d9c3e157e0c84a21bab2fbbea8b66955de2
Richard Chien <richardchienthebest@gmail.com>
2018-02-24 17:32:23 +0800

    Temporarily clean up all cqhttp staffs

--- src/api/api.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-#include "./types.h"
-
-void invoke_api(const std::string &action, const Params &params, ApiResult &result);
-void invoke_api(const std::string &action, const Params &params = {});
--- src/service/pushable_interface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-class IPushable {
-public:
-    virtual ~IPushable() = default;
-    virtual void push_event(const json &payload) const = 0;
-};
