a3dea2d89e8ecef785a66adbe0fa9a07d547214d
Richard Chien <richardchienthebest@gmail.com>
2018-03-01 16:35:48 +0800

    Fix script

--- scripts/generate.ps1
+++ scripts/generate.ps1
@@ -4,8 +4,9 @@ Set-Location .\build  # enter the build folder
 
 $vcpkg_root = "C:\CLI\vcpkg"
 $vcpkg_triplet = "x86-windows-static"
+$msvc_toolset = "v141"
 
-cmake -G "Visual Studio 15 2017" `
-    -DCMAKE_TOOLCHAIN_FILE=$vcpkg_root\scripts\buildsystems\vcpkg.cmake `
-    -DVCPKG_TARGET_TRIPLET=$vcpkg_triplet `
+cmake -G "Visual Studio 15 2017" -T "$msvc_toolset" `
+    -DCMAKE_TOOLCHAIN_FILE="$vcpkg_root\scripts\buildsystems\vcpkg.cmake" `
+    -DVCPKG_TARGET_TRIPLET="$vcpkg_triplet" `
     ..
