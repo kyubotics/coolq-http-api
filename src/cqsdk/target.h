a242a749b121f876b1e9809d5dd3ba78533c6b1e
Richard Chien <richardchienthebest@gmail.com>
2018-04-26 17:30:28 +0800

    Improve Target class

--- src/cqsdk/target.h
+++ src/cqsdk/target.h
@@ -8,17 +8,20 @@ namespace cq {
         std::optional<int64_t> group_id;
         std::optional<int64_t> discuss_id;
 
-        enum Type { PRIVATE, GROUP, DISCUSS };
+        enum Type { USER, GROUP, DISCUSS };
 
         Target() = default;
 
-        explicit Target(const int64_t user_id) : user_id(user_id) {}
-
-        explicit Target(const int64_t group_or_discuss_id, const Type type) {
-            if (type == GROUP) {
-                group_id = group_or_discuss_id;
-            } else if (type == DISCUSS) {
-                discuss_id = group_or_discuss_id;
+        explicit Target(const int64_t user_or_group_or_discuss_id, const Type type = USER) {
+            switch (type) {
+            case USER:
+                user_id = user_or_group_or_discuss_id;
+                break;
+            case GROUP:
+                group_id = user_or_group_or_discuss_id;
+                break;
+            case DISCUSS:
+                discuss_id = user_or_group_or_discuss_id;
             }
         }
 
@@ -26,5 +29,9 @@ namespace cq {
             : Target(group_or_discuss_id, type) {
             this->user_id = user_id;
         }
+
+        static Target user(const int64_t user_id) { return Target(user_id, USER); }
+        static Target group(const int64_t group_id) { return Target(group_id, GROUP); }
+        static Target discuss(const int64_t discuss_id) { return Target(discuss_id, DISCUSS); }
     };
 } // namespace cq
