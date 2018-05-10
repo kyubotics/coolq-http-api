fc6dc39a5e9bc54cc8f43d2e1a694a6a37606af0
Richard Chien <richardchienthebest@gmail.com>
2018-05-10 11:17:51 +0800

    Add console logging skeleton

---
+++ src/cqhttp/logging/handlers/console.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "cqhttp/logging/handler.h"
+
+#include <spdlog/logger.h>
+
+namespace cqhttp::logging {
+    struct ConsoleHandler : Handler {
+        void init() override;
+        void destroy() override;
+        void log(cq::logging::Level level, const std::string &tag, const std::string &msg) const override;
+
+    private:
+        std::shared_ptr<spdlog::logger> logger_ = nullptr;
+    };
+} // namespace cqhttp::logging
