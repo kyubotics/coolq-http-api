1f3c26511c156cd7435b8c196d9da38763f45b6d
Richard Chien <richardchienthebest@gmail.com>
2018-05-13 23:32:42 +0800

    Fix async actions

--- src/cqhttp/plugins/async_actions/async_actions.cpp
+++ src/cqhttp/plugins/async_actions/async_actions.cpp
@@ -8,18 +8,22 @@ namespace cqhttp::plugins {
     static const auto TAG = u8"异步动作";
 
     void AsyncActions::hook_missed_action(ActionContext &ctx) {
-        if (boost::algorithm::ends_with(ctx.action, "_async")) {
-            const auto action = ctx.action.substr(0, ctx.action.length() - strlen("_async"));
-            const auto ok = app.push_async_task([action, params = ctx.params] {
-                call_action(action, params);
-                logging::debug(TAG, u8"成功执行一个异步动作");
-            });
-            if (ok) {
-                logging::debug(TAG, u8"异步动作已进入全局线程池等待执行");
-                ctx.result.code = ActionResult::Codes::ASYNC;
-            } else {
-                logging::debug(TAG, u8"全局线程池无法执行异步动作，请尝试重启插件");
-                ctx.result.code = ActionResult::Codes::BAD_THREAD_POOL;
+        const auto suffix = "_async";
+        const auto suffix_len = strlen(suffix);
+        if (boost::algorithm::ends_with(ctx.action, suffix)) {
+            const auto action = ctx.action.substr(0, ctx.action.length() - suffix_len);
+            if (!boost::algorithm::ends_with(action, suffix)) {
+                const auto ok = app.push_async_task([action, params = ctx.params] {
+                    call_action(action, params);
+                    logging::debug(TAG, u8"成功执行一个异步动作");
+                });
+                if (ok) {
+                    logging::debug(TAG, u8"异步动作已进入全局线程池等待执行");
+                    ctx.result.code = ActionResult::Codes::ASYNC;
+                } else {
+                    logging::debug(TAG, u8"全局线程池无法执行异步动作，请尝试重启插件");
+                    ctx.result.code = ActionResult::Codes::BAD_THREAD_POOL;
+                }
             }
         }
 
