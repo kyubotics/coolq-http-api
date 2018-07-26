95245a0ee3e8446bc7a167da8868c007b86e39cd
Richard Chien <richardchienthebest@gmail.com>
2018-07-26 14:18:32 +0800

    Fix typos

--- src/cqhttp/plugins/web/websocket_reverse_client.cpp
+++ src/cqhttp/plugins/web/websocket_reverse_client.cpp
@@ -63,7 +63,7 @@ namespace cqhttp::plugins {
                 }
                 started_ = false;
             });
-            logging::info_success(TAG, u8"开启 WebSocket 反向客户端（" + name() + u8"）成功，开始连接 " + url_);
+            logging::info_success(TAG, u8"开启反向 WebSocket 客户端（" + name() + u8"）成功，开始连接 " + url_);
         }
     }
 
@@ -137,7 +137,7 @@ namespace cqhttp::plugins {
 
     void WebSocketReverse::EventClient::push_event(const json &payload) const {
         if (started_) {
-            logging::debug(TAG, u8"开始通过 WebSocket 反向客户端上报事件");
+            logging::debug(TAG, u8"开始通过反向 WebSocket 客户端上报事件");
 
             bool succeeded;
             try {
@@ -162,9 +162,9 @@ namespace cqhttp::plugins {
             }
 
             if (succeeded) {
-                logging::info_success(TAG, u8"通过 WebSocket 反向客户端上报数据到 " + url_ + u8" 成功");
+                logging::info_success(TAG, u8"通过反向 WebSocket 客户端上报数据到 " + url_ + u8" 成功");
             } else {
-                logging::warning(TAG, u8"通过 WebSocket 反向客户端上报数据到 " + url_ + u8" 失败");
+                logging::warning(TAG, u8"通过反向 WebSocket 客户端上报数据到 " + url_ + u8" 失败");
             }
         }
     }
