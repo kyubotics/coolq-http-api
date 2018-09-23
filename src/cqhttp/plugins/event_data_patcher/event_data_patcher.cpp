0081822f30027ec82cded6c3f0312062c13bb5e4
Richard Chien <richardchienthebest@gmail.com>
2018-09-23 21:24:53 +0800

    Add sender info in event data, fix #116

--- src/cqhttp/plugins/event_data_patcher/event_data_patcher.cpp
+++ src/cqhttp/plugins/event_data_patcher/event_data_patcher.cpp
@@ -9,6 +9,39 @@ namespace cqhttp::plugins {
             ctx.data["time"] = time(nullptr);
         }
 
+        if (ctx.data["post_type"] == "message") {
+            json sender = {
+                {"user_id", ctx.data["user_id"]},
+            };
+
+            if (ctx.data["message_type"] == "group") {
+                try {
+                    auto info = cq::api::get_group_member_info(ctx.data["group_id"], ctx.data["user_id"]);
+                    sender.update({
+                        {"nickname", info.nickname},
+                        {"card", info.card},
+                        {"sex", info.sex},
+                        {"age", info.age},
+                    });
+                } catch (const cq::exception::ApiError &) {
+                }
+            }
+
+            if (sender.count("nickname") == 0) {
+                try {
+                    auto info = cq::api::get_stranger_info(ctx.data["user_id"]);
+                    sender.update({
+                        {"nickname", info.nickname},
+                        {"sex", info.sex},
+                        {"age", info.age},
+                    });
+                } catch (const cq::exception::ApiError &) {
+                }
+            }
+
+            ctx.data.emplace("sender", sender);
+        }
+
         ctx.next();
     }
 } // namespace cqhttp::plugins
