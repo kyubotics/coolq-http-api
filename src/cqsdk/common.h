4da94fc3b6362314b9a11b31dd6c2c99c5eb31c3
Richard Chien <richardchienthebest@gmail.com>
2018-10-16 21:46:40 +0800

    Temporarily remove cqsdk

--- src/cqsdk/common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <algorithm>
-#include <boost/algorithm/string.hpp>
-#include <cstdint>
-#include <functional>
-#include <optional>
-#include <string>
--- src/cqsdk/def.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define __CQ_EVENT(ReturnType, Name, Size)                                                            \
-    __pragma(comment(linker, "/EXPORT:" #Name "=_" #Name "@" #Size)) extern "C" __declspec(dllexport) \
-        ReturnType __stdcall Name
