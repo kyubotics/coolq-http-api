86dea305068b32001311acf8108693653d7b3196
Richard Chien <richardchienthebest@gmail.com>
2020-03-29 14:28:39 +0800

    remove update source "gitee", "coding" and "china"

--- src/cqhttp/core/helpers.cpp
+++ src/cqhttp/core/helpers.cpp
@@ -6,14 +6,10 @@ using namespace std;
 
 namespace cqhttp::helpers {
     string get_update_source_url(string rel_path) {
-        auto url = app.config().get_string("update_source", "china");
+        auto url = app.config().get_string("update_source", "global");
 
         if (url == "global" || url == "github") {
             url = "https://raw.githubusercontent.com/richardchien/coolq-http-api-release/master/";
-        } else if (url == "china" || url == "coding") {
-            url = "https://coding.net/u/richardchien/p/coolq-http-api-release/git/raw/master/";
-        } else if (url == "gitee") {
-            url = "https://gitee.com/richardchien/coolq-http-api-release/raw/master/";
         }
 
         if (!boost::ends_with(url, "/")) {
