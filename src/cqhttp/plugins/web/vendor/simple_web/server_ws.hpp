c00370c572394556270c4840781d80cefaa1cd86
Richard Chien <richardchienthebest@gmail.com>
2020-04-19 14:25:04 +0800

    fix 2

--- src/cqhttp/plugins/web/vendor/simple_web/server_ws.hpp
+++ src/cqhttp/plugins/web/vendor/simple_web/server_ws.hpp
@@ -730,8 +730,10 @@ namespace SimpleWeb {
             connection->set_timeout();
 
             // Send pong
-            auto empty_send_stream = std::make_shared<OutMessage>();
-            connection->send(empty_send_stream, nullptr, fin_rsv_opcode + 1);
+            // cqhttp change: fix standard conformance (which allows application data in ping frame)
+            auto out_message = std::make_shared<OutMessage>();
+            *out_message << in_message->string();
+            connection->send(out_message, nullptr, fin_rsv_opcode + 1);
 
             if(endpoint.on_ping)
               endpoint.on_ping(connection);
