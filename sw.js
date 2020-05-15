5ada3d988af873e906de3ec0a69727ee284845ff
bleatingsheep <35132976+bltsheep@users.noreply.github.com>
2020-05-16 04:00:08 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,6 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js'
 )
 
-const ALLOWED_HOSTS = [
-  'cqhttp.cc',
-  'cqhttp.bleatingsheep.org',
-  'img.shields.io',
-  'raw.githubusercontent.com',
-  'unpkg.com'
-]
-
 const matchCb = ({ url, event }) => {
   return true
 }
